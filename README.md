906b4486ed38273092ad1196bdb07fb4945c59aa
Daniel Melo Alencar <danielmalencar2011@gmail.com>
2021-09-22 04:47:02 -0300

    Projeto completo

--- README.md
+++ README.md
@@ -1,24 +1,80 @@
-# snack-lab
+# VueJS + Monaco Editor + Expo Snack SDK [![Codacy Badge](https://api.codacy.com/project/badge/Grade/26e36b4144c1415e93d3ee398a785e05)](https://app.codacy.com/gh/danielmeloalencar/vue-monaco-expo-snack-sdk?utm_source=github.com&utm_medium=referral&utm_content=danielmeloalencar/vue-monaco-expo-snack-sdk&utm_campaign=Badge_Grade_Settings)
 
-## Project setup
+### Este projeto é parte de um projeto maior
+
+<!---Esses são exemplos. Veja https://shields.io para outras pessoas ou para personalizar este conjunto de escudos. Você pode querer incluir dependências, status do projeto e informações de licença aqui--->
+
+<img src="screenshot.jpg" alt="exemplo imagem">
+
+> Não é necessário instalar Expo, React Native, Android Studio nem Android SDK
+
+> Atualiza o app no celular em tempo real (Hot Reload)
+
+> Atualiza as dependências em tempo real, inclusive as sub-dependências
+
+> Gera link para download do projeto completo em pasta compactada. O link é gerado apenas no console, pois não havia necessidade de criar uma UI para isso.
+
+## 💻 Pré-requisitos
+
+Antes de começar, verifique se você atendeu aos seguintes requisitos:
+<!---Estes são apenas requisitos de exemplo. Adicionar, duplicar ou remover conforme necessário--->
+* `Node`
+
+
+## 🚀 Instalando o projeto
+
+Para instalar, siga estas etapas:
+
+Windows / Linux:
 ```
-npm install
+$ npm install 
 ```
 
-### Compiles and hot-reloads for development
+
+## ☕ Rodando o projeto
+
+Para rodar, siga estas etapas:
+
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
+Para compilar, siga estas etapas:
+
 ```
 npm run build
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
+## 📫 Contribuindo para Eagle IDE
+<!---Se o seu README for longo ou se você tiver algum processo ou etapas específicas que deseja que os contribuidores sigam, considere a criação de um arquivo CONTRIBUTING.md separado--->
+Para contribuir com Eagle IDE, siga estas etapas:
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+1. Fork it!
+2. Crie um branch: `git checkout -b my-new-feature`.
+3. Faça suas alterações e confirme-as: `git commit -am 'Add some feature'`
+4. Envie para o branch original: `git push origin my-new-feature`
+5. Envie uma pull request.
+
+Como alternativa, consulte a documentação do GitHub em [como criar uma solicitação pull](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## 🤝 Colaboradores
+
+Agradecemos às seguintes pessoas que contribuíram para este projeto:
+
+<table>
+  <tr>
+      <td align="center">
+      <a href="#">
+        <img src="daniel-alencar.jpg" width="100px;" alt="Foto do Mark Zuckerberg"/><br>
+        <sub>
+          <b>Daniel Melo Alencar</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
+
+
+## 😄 Seja um dos contribuidores<br>
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,209 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="esquerda">
+      <h3>Protótipo de Editor em Tempo Real (React Native/ Expo)</h3>
+      <h4>
+        Modifique o código abaixo e visualize as alterações em tempo real no seu
+        celular
+      </h4>
+      <MonacoEditor
+        v-if="isShow"
+        @change="atualizarCodigo"
+        theme="vs-dark"
+        :diffEditor="false"
+        language="javascript"
+        :original="codigo"
+        class="editor"
+        :value="codigo"
+        v-model="codigo"
+      ></MonacoEditor>
+      <textarea
+        style="width:595px;height:30px"
+        v-model="dependencias"
+      ></textarea>
+      <br />
+      <button @click="atualizarDependencias()">Atualizar dependências</button>
+      {{ msg }}
+      <h4>VueJS / Monaco Editor / Expo Snack SDK</h4>
+    </div>
+
+    <div class="direita">
+      <h3>Aponte a câmera do seu celular para o QR CODE</h3>
+      <qrcode-vue :value="url" :size="size" level="H" v-if="url" />
+
+      <div style="">Dispositivos conectados: {{ dispositivos }}</div>
+
+      <div>Versão EXPO: {{ SDK }}</div>
+      <h4>Desenvolvido por Daniel Melo Alencar</h4>
+      <a href="https://expo.dev/client" target="blank"
+        >Baixe o app cliente aqui
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Snack } from "snack-sdk";
 
+import QrcodeVue from "qrcode.vue";
+import MonacoEditor from "vue-monaco"; //https://github.com/egoist/vue-monaco
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    QrcodeVue,
+    MonacoEditor,
+  },
+  data() {
+    return {
+      isShow: false,
+      SDK: 0,
+      dispositivos: 0,
+      snack: "",
+      url: null,
+      size: 200,
+      id: "",
+      msg: "",
+      dependencias: `{"expo-status-bar":{"version":"~1.0.4"}}`,
+      codigo: `
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    paddingTop: 50,
+    justifyContent:'center',
+    alignItems:'center' 
+  },
+});
+
+export default function App() {
+   return (
+    <View style={styles.container}>
+      <Text style={{fontSize:30}} >This is magic </Text>
+    </View>
+  );
+}      
+ `,
+    };
+  },
+  methods: {
+    pegaURL: async function() {
+      // Create Snack
+      this.snack = new Snack({
+        online: true,
+        codeChangesDelay: 500,
+        name: "Meu App",
+        dependencies: {},
+      });
+
+      // Make the Snack available online
+      this.snack.setOnline(true);
+
+      this.snack.getStateAsync().then((response) => {
+        this.url = response.url;
+        this.SDK = response.sdkVersion;
+      });
+
+      this.snack.addStateListener((state, prevState) => {
+        if (state.connectedClients !== prevState.connectedClients) {
+          this.dispositivos = Object.keys(state.connectedClients).length;
+          for (const key in state.connectedClients) {
+            if (!prevState.connectedClients[key]) {
+              console.log(
+                "A client has connected! " +
+                  state.connectedClients[key].platform
+              );
+            }
+          }
+        }
+      });
+    },
+
+    atualizarCodigo: async function() {
+      this.snack.updateFiles({
+        "App.js": {
+          type: "CODE",
+          contents: this.codigo,
+        },
+      });
+
+      this.snack.getStateAsync().then((response) => {
+        console.log(response);
+      });
+      const { id } = await this.snack.saveAsync();
+      console.log(`Saved with id ${id}`);
+
+      const url = await this.snack.getDownloadURLAsync();
+      console.log("Download URL: " + url);
+    },
+
+    pegaInfo: async function() {
+      //await this.snack.setName('MEUAPP')
+      this.snack.getStateAsync().then((response) => {
+        /// this.url = response.url;
+        console.log(response);
+      });
+    },
+    atualizarDependencias: async function() {
+      this.msg = "Atualizando dependências...";
+      this.snack.updateDependencies(JSON.parse(this.dependencias.trim()));
+      const { dependencies } = await this.snack.getStateAsync();
+      var erro = false;
+      for (const name in dependencies) {
+        if (dependencies[name].handle === undefined) {
+          erro = true;
+          alert("Não foi possível resolver a dependencia " + name);
+          console.log(dependencies[name].handle); // string -> https://...
+        }
+      }
+      /** RESOLVE AS DEPENDENCIAS PERDIDAS */
+
+      const depend = {};
+      for (const name in this.snack.getState().missingDependencies) {
+        depend[name] = {
+          version: this.snack.getState().missingDependencies[name]
+            .wantedVersion,
+        };
+        console.log("Instalando ependencia perdida: " + name);
+      }
+      console.log(depend);
+      this.snack.updateDependencies(depend);
+
+      /**************************** */
+
+      this.msg = erro ? "Erro" : "Concluído";
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.isShow = true;
+    });
+    this.pegaURL();
+  },
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  font-family: "Tahoma", Courier, monospace;
+  color: rgb(92, 92, 92);
+}
+
+.esquerda {
+  flex: 1;
+}
+
+.direita {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+}
+
+.editor {
+  width: 600px;
+  height: 350px;
 }
 </style>
